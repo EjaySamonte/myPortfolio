38344f64a0ecb86e2a8477ca264e25d4e510a98c
EjaySamonte <ericjayangelo@gmail.com>
2026-02-28 11:33:33 +0800

    updated flashcard input

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Station Supervisor', 'Junior Auditor', 'Frontend Developer'],
+    strings: ['Station Supervisor', 'Junior Auditor', 'Junior Frontend Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
